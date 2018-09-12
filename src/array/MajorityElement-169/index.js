8ffc3132d5d110f62397c58b7d83245e4f524ca3
gt.zhuang.htx <gt.zhuang.htx@gmail.com>
2018-09-12 20:51:53 +0800

    49 and 169

---
+++ src/array/MajorityElement-169/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    const map = {};
+    let maxTime = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let currentNum = nums[i];
+        map[currentNum] = (map[currentNum] || 0) + 1;
+        maxTime = Math.max(maxTime, map[currentNum]);
+    }
+    return parseInt(Object.keys(map).find(key => map[key] === maxTime));
+};
+
+module.exports = majorityElement;
