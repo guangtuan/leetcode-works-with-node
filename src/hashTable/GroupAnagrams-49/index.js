c8efdf84d037f406923c4d47f7cbb60618ad1294
guangtuan <zhuangguangtuan@gmail.com>
2018-09-12 20:51:53 +0800

    49 and 169

---
+++ src/hashTable/GroupAnagrams-49/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    return Object.values(strs.reduce((acc, curr) => {
+        const sortedCurr = curr.split("").sort();
+        if (acc[sortedCurr]) {
+            acc[sortedCurr].push(curr);
+        } else {
+            acc[sortedCurr] = [curr];
+        }
+        return acc;
+    }, {})).map(childArray => childArray.sort());
+};
+
+module.exports = groupAnagrams;
