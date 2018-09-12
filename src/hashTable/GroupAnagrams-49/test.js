c75d75d1e93535529d6ba08f90d6567e81f7fa34
gt.zhuang.htx <gt.zhuang.htx@gmail.com>
2018-09-12 20:51:53 +0800

    49 and 169

---
+++ src/hashTable/GroupAnagrams-49/test.js
@@ -0,0 +1,24 @@
+const testCases = [
+    {
+        input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+        output: [
+            ["ate", "eat", "tea"],
+            ["nat", "tan"],
+            ["bat"]
+        ]
+    }
+];
+
+const assert = require('assert');
+const resolve = require('./index');
+
+describe('group anagrams', () => {
+    testCases.forEach(({input, output}) => {
+        it('', () => {
+            assert.deepEqual(
+                output,
+                resolve(input)
+            );
+        });
+    });
+});
