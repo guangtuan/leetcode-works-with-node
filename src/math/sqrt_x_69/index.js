532d9e49cb4f11b9dcc014e9374278cdfe8bf70f
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-03 13:28:11 +0800

    math and stack

---
+++ src/math/sqrt_x_69/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    for (let i = 0; i <= x; i++) {
+        if ((i * i <= x) && ((i + 1) * (i + 1) > x)) {
+            return i;
+        }
+    }
+};
+
+mySqrt = function (x) {
+    const result = Math.sqrt(x);
+    return Math.floor(result);
+}
+
+module.exports = mySqrt;
