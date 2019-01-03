975b63b2874675ac462df5b87bec34c309443a3a
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-03 13:28:11 +0800

    math and stack

---
+++ src/stack/valid-parentheses-20/test.js
@@ -0,0 +1,33 @@
+const resolve = require('./index');
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: '()',
+        output: true
+    },
+    {
+        input: '()[]{}',
+        output: true
+    },
+    {
+        input: '(]',
+        output: false
+    },
+    {
+        input: '([)]',
+        output: false
+    },
+    {
+        input: '{[]}',
+        output: true
+    }
+];
+
+describe("valid-parentheses-20", () => {
+    testCases.forEach(({ input, output }) => {
+        it(`input is ${input}, output is ${output}`, () => {
+            assert.equal(resolve(input), output);
+        })
+    });
+});
