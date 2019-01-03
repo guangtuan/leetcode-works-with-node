574aa996fe27c4dd9698e6b5643bfc174f451365
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-03 13:28:11 +0800

    math and stack

---
+++ src/stack/valid-parentheses-20/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const map = {
+        '[': ']',
+        '{': '}',
+        '(': ')'
+    };
+    const stack = [];
+    for (const current of s) {
+        if (map[current]) {
+            stack.push(current);
+            continue;
+        }
+        if (map[stack[stack.length - 1]] === current) {
+            stack.pop();
+            continue;
+        }
+        return false;
+    }
+    return stack.length === 0;
+};
+
+module.exports = isValid;
