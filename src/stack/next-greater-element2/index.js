a5ac5f94693e8d2944b10c6e43022684a362b0de
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-13 16:48:52 +0800

    next greater element

---
+++ src/stack/next-greater-element2/index.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function (nums) {
+    const max = Math.max(nums);
+    return nums.reduce((acc, num, currentIndex, array) => {
+        if (num === max) {
+            acc[currentIndex] = -1;
+            return acc;
+        }
+        for (let i = currentIndex + 1; i < array.length + 1; i++) {
+            if (array[i] > num) {
+                acc[currentIndex] = array[i];
+                return acc;
+            }
+        }
+        if (typeof acc[currentIndex] === "undefined") {
+            for (let j = 0; j < currentIndex; j++) {
+                if (array[j] > num) {
+                    acc[currentIndex] = array[j];
+                    return acc;
+                }
+            }
+        }
+        if (typeof acc[currentIndex] === "undefined") {
+            acc[currentIndex] = -1;
+        }
+        return acc;
+    }, []);
+};
+
+module.exports = nextGreaterElements;
