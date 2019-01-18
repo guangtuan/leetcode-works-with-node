7103ab83339413abe6b2f5ac3ae27832e4f65d83
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-18 08:57:34 +0800

    biz / stack

---
+++ src/bit/buddyStrings_859/test.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const resolve = require('./index');
+
+const testCases = [
+    {
+        input: {
+            A: 'ab',
+            B: 'ba'
+        },
+        output: true
+    },
+    {
+        input: {
+            A: 'ab',
+            B: 'ab'
+        },
+        output: false
+    },
+    {
+        input: {
+            A: 'aa',
+            B: 'aa'
+        },
+        output: true
+    },
+    {
+        input: {
+            A: 'aaaaaaabc',
+            B: 'aaaaaaacb'
+        },
+        output: true
+    },
+    {
+        input: {
+            A: '',
+            B: 'aa'
+        },
+        output: false
+    },
+    {
+        input: {
+            A: 'abab',
+            B: 'abab'
+        },
+        output: true
+    }
+];
+
+const run = ({ input, output }) => {
+    it(`input is ${JSON.stringify(input)}, expected output is ${output}`, () => {
+        assert.equal(resolve(input.A, input.B), output);
+    });
+};
+
+describe("buddy Strings", () => {
+    testCases.forEach(run);
+});
---
+++ src/stack/minAddToMakeValid_921/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @return {number}
+ */
+var minAddToMakeValid = function (S) {
+    const stack = [];
+    for (let c of S) {
+        if (c === '(') {
+            stack.push(c);
+            continue;
+        }
+        if (stack[stack.length - 1] === '(') {
+            stack.pop();
+            continue;
+        }
+        stack.push(c);
+    }
+    return stack.length;
+};
+
+module.exports = minAddToMakeValid;
