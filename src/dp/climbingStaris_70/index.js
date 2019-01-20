86b94cef14c98d37b0eb660df22cc39f5bc6e408
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-20 14:57:16 +0800

    climbing stairs

---
+++ src/dp/climbingStaris_70/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const results = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        results[i] = results[i - 1] + results[i - 2];
+    }
+    return results[n];
+};
+
+module.exports = climbStairs;
