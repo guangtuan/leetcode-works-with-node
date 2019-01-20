ee953e42dc9977fc11271c5f23eb0b84d7ff177d
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-01-20 14:57:16 +0800

    climbing stairs

---
+++ src/dp/climbingStaris_70/test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const resolve = require('./index');
+
+const testCases = [
+    {
+        input: 2,
+        output: 2
+    },
+    {
+        input: 3,
+        output: 3
+    },
+    {
+        input: 4,
+        output: 5
+    },
+    {
+        input: 5,
+        output: 8
+    }
+];
+
+const run = ({ input, output }) => {
+    it(`input is ${input}, expected output is ${output}`, () => {
+        assert.equal(resolve(input), output);
+    });
+};
+
+describe("climbing stairs", () => {
+    testCases.forEach(run);
+});
