6d1c434698fccb78794509a93d085ceabe9164e2
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-02-05 14:16:53 +0800

    src/array/find_the_duplicate_number_287/

---
+++ src/array/find_the_duplicate_number_287/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+    while (nums[nums[0]] != nums[0]) {
+        [nums[nums[0]], nums[0]] = [nums[0], nums[nums[0]]]
+    }
+    return nums[0]
+};
+
+module.exports = findDuplicate;
