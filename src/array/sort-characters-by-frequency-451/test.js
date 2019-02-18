a9b3f899cbb06d41dd60a806bc5f387d85cc4b91
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-02-18 09:03:08 +0800

    sort chars by freq

---
+++ src/array/sort-characters-by-frequency-451/test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const resolve = require('./index');
+
+const testCases = [
+    {
+        input: "tree",
+        output: "eetr"
+    },
+    {
+        input: "cccaaa",
+        output: "cccaaa"
+    },
+    {
+        input: "Aabb",
+        output: "bbAa"
+    }
+];
+
+const run = ({ input, output }) => {
+    it(`input is ${input}, expected output is ${output}`, () => {
+        assert.equal(resolve(input), output);
+    });
+};
+
+describe("find the duplicate number", () => {
+    testCases.forEach(run);
+});
