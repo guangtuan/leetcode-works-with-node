8372713998c3a10f0c1b2eeb75c14e4a1f23621f
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-02-18 09:03:08 +0800

    sort chars by freq

---
+++ src/array/sort-characters-by-frequency-451/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+    let map = {};
+    for (let c of s) {
+        map[c] = (map[c] || 0) + 1;
+    }
+    let pairs = Object.entries(map).sort(([charA, countA], [charB, countB]) => countB - countA);
+    let result = '';
+    for (let [char, count] of pairs) {
+        result = result + char.repeat(count);
+    }
+    return result;
+};
+
+module.exports = frequencySort;
