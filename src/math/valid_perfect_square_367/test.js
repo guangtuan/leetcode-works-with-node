17521295bb88c1353dd73c141aeee41b4a9faa59
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-04-19 22:28:23 +0800

    valid_perfect_square_367

---
+++ src/math/valid_perfect_square_367/test.js
@@ -0,0 +1,33 @@
+const resolve = require('./index');
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: 16,
+        output: true
+    },
+    {
+        input: 14,
+        output: false
+    },
+    {
+        input: 15 * 15,
+        output: true
+    },
+    {
+        input: 22555 * 22555,
+        output: true
+    },
+    {
+        input: 14 * 14,
+        output: true
+    }
+];
+
+describe("valid perfect square", () => {
+    testCases.forEach(({ input, output }) => {
+        it(`input is ${input} and output is ${output}`, () => {
+            assert.equal(resolve(input), output);
+        });
+    });
+});
