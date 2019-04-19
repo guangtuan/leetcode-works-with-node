abead2ca72b107f993463ac3d976b2afe0c7f06a
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-04-19 22:26:15 +0800

    two sum

---
+++ src/array/two-sum-01/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (typeof map[num] !== "undefined") {
+            return [map[num], i];
+        }
+        map[target - num] = i;
+    }
+    return [];
+};
+
+module.exports = twoSum;
