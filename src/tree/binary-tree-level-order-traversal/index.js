bbbb612192c4f13272ef6f59508b04e387c6b1e5
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-04-23 22:10:36 +0800

    binary-tree-level-order-traversal

---
+++ src/tree/binary-tree-level-order-traversal/index.js
@@ -0,0 +1,34 @@
+var levelOrder = function (root) {
+    const colletors = [];
+    function walk(cursors) {
+        if (cursors.length === 0) {
+            return;
+        }
+        const next = [];
+        const currentLevel = [];
+        for (let cursor of cursors) {
+            currentLevel.push(cursor.val);
+            if (cursor.left !== null) {
+                next.push(cursor.left);
+            }
+            if (cursor.right !== null) {
+                next.push(cursor.right);
+            }
+        }
+        colletors.push(currentLevel);
+        walk(next);
+    }
+    if (root !== null) {
+        walk([root]);
+    }
+    return colletors;
+}
+
+// colletors.push(cursors.map(({ val }) => val));
+// walk(cursors
+//     .map(({ left, right }) => ([left, right]))
+//     .reduce((acc, currentArray) => acc.concat(currentArray), [])
+//     .filter(_ => _ !== null)
+// );
+
+module.exports = levelOrder;
