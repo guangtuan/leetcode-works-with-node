758f81acba34797263a4bdd883d60dd7d1e3d115
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-05-05 20:31:29 +0800

    longest-substring-without-repeating-characters-3

---
+++ src/string/longest-substring-without-repeating-characters-3/test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const resolve = require('./index');
+
+const testCases = [
+    {
+        input: "abcabcbb",
+        output: 3
+    },
+    {
+        input: "bbbbb",
+        output: 1
+    },
+    {
+        input: "pwwkew",
+        output: "3"
+    },
+    {
+        input: "",
+        output: 0
+    },
+    {
+        input: " ",
+        output: 1
+    },
+    {
+        input: "au",
+        output: 2
+    }
+];
+
+const run = ({ input, output }) => {
+    it(`input is ${input}, expected output is ${output}`, () => {
+        assert.equal(resolve(input), output);
+    });
+};
+
+describe("#3.Longest Substring Without Repeating Characters", () => {
+    testCases.forEach(run);
+});
