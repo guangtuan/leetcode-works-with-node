eb3c6207b967553d9411be41f4ab6e14c01e189e
gt.zhuang.htx <gt.zhuang.htx@gmail.com>
2019-05-10 17:09:50 +0800

    pascal triangle two

---
+++ src/array/pascal-triangle-two-119/test.js
@@ -0,0 +1,33 @@
+const testCases = [
+    {
+        input: 0,
+        output: [1]
+    },
+    {
+        input: 1,
+        output: [1, 1]
+    },
+    {
+        input: 2,
+        output: [1, 2, 1]
+    },
+    {
+        input: 3,
+        output: [1, 3, 3, 1]
+    },
+    {
+        input: 4,
+        output: [1, 4, 6, 4, 1]
+    }
+];
+
+const assert = require('assert');
+const resolve = require('./index');
+
+describe("#119.Pascal's Triangle II", () => {
+    testCases.forEach(({ input, output }) => {
+        it(`input is ${input} and output is ${output}`, () => {
+            assert.deepEqual(resolve(input), output);
+        });
+    });
+});
