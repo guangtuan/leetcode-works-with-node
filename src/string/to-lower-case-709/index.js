b5106a06dc288ad0f303c1f07acae31784b856f4
guangtuan <zhuangguangtuan@gmail.com>
2019-05-17 17:26:36 +0800

    number 709: to lower case

---
+++ src/string/to-lower-case-709/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+    const A = "A".charCodeAt(0);
+    const Z = "Z".charCodeAt(0);
+    const a = "a".charCodeAt(0);
+    const delta = A - a;
+    const convert = singleChar => {
+        const c = singleChar.charCodeAt(0);
+        if (c <= Z && c >= A) {
+            return String.fromCharCode(c - delta);
+        }
+        return singleChar;
+    }
+    return str.split("").map(convert).join("");
+};
+
+module.exports = toLowerCase;
