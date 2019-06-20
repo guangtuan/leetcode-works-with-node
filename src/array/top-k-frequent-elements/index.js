e970bca9c38ad3bca831d000b02bfbaef6551337
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-06-20 14:35:06 +0800

    number347, top k frequent elements

---
+++ src/array/top-k-frequent-elements/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+    const map = nums.reduce((acc, num) => {
+        acc[num] = (acc[num] || 0) + 1;
+        return acc;
+    }, {});
+    const buckets = [];
+    for (let [num, feq] of Object.entries(map)) {
+        if (!buckets[feq]) {
+            buckets[feq] = [num];
+        } else {
+            buckets[feq].push(num);
+        }
+    }
+    const result = [];
+    while (result.length < k) {
+        result.push(...(buckets.pop() || []));
+    }
+    return result;
+};
+
+module.exports = topKFrequent;
