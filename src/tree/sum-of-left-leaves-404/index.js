53150e7cf1c6556109ab4038e0388143d7fccbce
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-07-11 17:32:08 +0800

    number 404 ac

---
+++ src/tree/sum-of-left-leaves-404/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root) {
+    let result = 0;
+    const walk = (node, side) => {
+        if (node.left === null && node.right === null) {
+            if (side === 'left') {
+                result += node.val;
+            }
+            return;
+        }
+        if (node.right !== null) {
+            walk(node.right, 'right');
+        }
+        if (node.left !== null) {
+            walk(node.left, 'left');
+        }
+    };
+    if (root === null) {
+        return 0;
+    }
+    walk(root);
+    return result;
+};
+
+module.exports = sumOfLeftLeaves;
