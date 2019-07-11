eef5725f093c76f3fb59accc90975db19a3b7078
zhuangguangtuan <zhuangguangtuan@gmail.com>
2019-07-11 16:20:03 +0800

    number 96

---
+++ src/tree/unique-binary-search-trees-96/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: 3,
+        output: 5
+    }
+];
+
+const resolve = require('./index');
+
+describe('96. Unique Binary Search Trees', () => {
+    testCases.forEach(({ input, output }) => {
+        it(`input is ${input}, output is ${output}`, () => {
+            assert.equal(output, resolve(input));
+        });
+    });
+});
