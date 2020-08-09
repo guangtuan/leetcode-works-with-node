158983ba6171d4a1567882009cba14b63cfe6eec
guangtuan <zhuangguangtuan@gmail.com>
2020-08-09 17:23:50 +0800

    resolve 100. Same Tree

--- DataStructure/Tree/create.js
+++ DataStructure/Tree/create.js
@@ -29,7 +29,83 @@ const horizontalBuildBST = args => {
     return nodes[0];
 }
 
+const horizontalBuildBSTWithNullNodeVal = args => {
+    let nodes = [new TreeNode(args[0])];
+    for (let i = 1; i < args.length; i++) {
+        let currentNode = new TreeNode(args[i]);
+        nodes.push(currentNode);
+        if (i % 2 === 1) {
+            nodes[(i - 1) / 2].left = currentNode;
+        } else {
+            nodes[(i - 2) / 2].right = currentNode;
+        }
+    }
+    return nodes[0];
+}
+
+function TreeNode2(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+const arrayToBinaryTree = (arr) => {
+    if (arr === undefined || arr.length === 0) {
+        return null;
+    }
+
+    const root = new TreeNode2(arr[0]);
+    const queue = [root];
+
+    let isLeft = true; // to be the left children or the right children
+    arr.slice(1).forEach((val) => {
+        const curr = val !== null ? new TreeNode2(val) : null;
+        if (isLeft) {
+            queue[0].left = curr;
+        } else {
+            queue[0].right = curr;
+            queue.shift();
+        }
+
+        if (curr !== null) {
+            queue.push(curr);
+        }
+
+        isLeft = !isLeft;
+    });
+
+    return root;
+};
+
+const prettyPrintBinaryTree = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+        process.stdout.write('Empty tree\n');
+        return;
+    }
+
+    if (node.right) {
+        prettyPrintBinaryTree(
+            node.right,
+            prefix + (isLeft ? '│   ' : '    '),
+            false
+        );
+    }
+
+    process.stdout.write(prefix + (isLeft ? '└── ' : '┌── ') + node.val + '\n');
+
+    if (node.left) {
+        prettyPrintBinaryTree(
+            node.left,
+            prefix + (isLeft ? '    ' : '│   '),
+            true
+        );
+    }
+};
+
 module.exports = {
     sortedArrayToBST: sortedArrayToBST,
-    horizontalBuildBST: horizontalBuildBST
+    horizontalBuildBST: horizontalBuildBST,
+    horizontalBuildBSTWithNullNodeVal: horizontalBuildBSTWithNullNodeVal,
+    prettyPrintBinaryTree: prettyPrintBinaryTree,
+    arrayToBinaryTree: arrayToBinaryTree
 }
---
+++ src/tree/same-tree/test.js
@@ -0,0 +1,54 @@
+const { horizontalBuildBSTWithNullNodeVal, prettyPrintBinaryTree } = require('../../../DataStructure/Tree/create');
+const resolve = require('./index');
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: {
+            left: [1, 2, 3],
+            right: [1, 2, 3]
+        },
+        output: true
+    },
+    {
+        input: {
+            left: [1, 2],
+            right: [1, null, 2]
+        },
+        output: false
+    },
+    {
+        input: {
+            left: [1, 2, 1],
+            right: [1, 1, 2]
+        },
+        output: false
+    },
+    {
+        input: {
+            left: [10, 5, 15],
+            right: [10, 5, null, null, 15]
+        },
+        output: false
+    },
+    {
+        input: {
+            left: [1, 2],
+            right: [1, null, 2]
+        },
+        output: false
+    }
+];
+
+describe('#100. Same Tree', () => {
+    testCases.forEach(({ input: { left, right }, output }) => {
+        it(`${output}`, () => {
+            const realLeft = horizontalBuildBSTWithNullNodeVal(left);
+            const realRight = horizontalBuildBSTWithNullNodeVal(right);
+            prettyPrintBinaryTree(realLeft);
+            console.log("");
+            prettyPrintBinaryTree(realRight);
+            assert.equal(resolve(realLeft, realRight), output);
+        });
+    });
+});
