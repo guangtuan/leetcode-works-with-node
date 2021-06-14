9ff994801da220137a66853f2d1f4f541dd7768c
guangtuan <zhuangguangtuan@gmail.com>
2021-06-14 15:32:42 +0800

    coin change 322 dp resolve

---
+++ src/dp/coin_change_322/index.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+    const map = {}
+    let fn = n => {
+        if (n < 0) {
+            return -1;
+        }
+        if (n === 0) {
+            return 0;
+        }
+        if (map[n]) {
+            return map[n];
+        }
+        let res = -1;
+        for (let coin of coins) {
+            const got = fn(n - coin) + 1;
+            if (got === 0) {
+                continue;
+            }
+            if (res === -1) {
+                res = got;
+            }
+            if (res !== -1) {
+                res = Math.min(res, got);
+            }
+        }
+        map[n] = res;
+        return map[n];
+    }
+    return fn(amount);
+};
+
+module.exports = coinChange;
