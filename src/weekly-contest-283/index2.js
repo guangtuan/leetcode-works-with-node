9e942736bc965f11fdaaa22c24b68676f0153834
guangtuan <zhuangguangtuan@gmail.com>
2022-03-06 18:50:31 +0800

    解决#6017的超时问题

--- src/weekly-contest-283/index2.js
+++ src/weekly-contest-283/index2.js
@@ -3,18 +3,22 @@
  * @param {number} k
  * @return {number}
  */
-var minimalKSum = function(nums, k) {
-    const ignore = new Set(nums);
-    let result = 0;
-    let number = 1;
-    while (k > 0) {
-        if (!ignore.has(number)) {
-            result += number;
-            k = k - 1;
+var minimalKSum = function (nums, k) {
+    nums.sort((n1, n2) => n1 - n2);
+    const intersection = new Set();
+    let cursor = k;
+    for (let num of nums) {
+        if (num <= cursor) {
+            if (!intersection.has(num)) {
+                intersection.add(num);
+                cursor = cursor + 1;
+            }
         }
-        number++;
     }
-    return result;
+    const total = n => (n + 1) * n / 2;
+    const sum = total(cursor);
+    const toRemove = [...intersection].reduce((acc, curr) => acc + curr, 0);
+    return sum - toRemove;
 };
 
 module.exports = minimalKSum;
--- src/weekly-contest-283/test2.js
+++ src/weekly-contest-283/test2.js
@@ -3,6 +3,17 @@ const resolve = require("./index2");
 
 const testCases = [
     {
+        input: {
+            nums: [93, 44, 49, 45, 93, 52, 6, 7, 88, 70, 86, 15, 38, 86, 86, 95, 8, 62, 13, 84, 26, 16, 33, 85, 7, 62, 55, 50, 77, 10, 76, 10, 35, 67, 19, 12, 24, 39, 76, 37],
+            k: 17
+        },
+        output: 250
+    },
+    {
+        input: { nums: [96, 44, 99, 25, 61, 84, 88, 18, 19, 33, 60, 86, 52, 19, 32, 47, 35, 50, 94, 17, 29, 98, 22, 21, 72, 100, 40, 84], k: 35 },
+        output: 794
+    },
+    {
         input: { nums: [1, 4, 25, 10, 25], k: 2 },
         output: 5
     },
@@ -12,10 +23,18 @@ const testCases = [
     }
 ];
 
-describe('#2rd', () => {
-    testCases.forEach(({ input, output }) => {
-        it(`input is ${JSON.stringify(input)}, output is ${output}`, () => {
-            assert.equal(resolve(input.nums, input.k), output);
-        });
-    });
-});
+describe(
+    '#2rd',
+    () => {
+        testCases.forEach(
+            ({ input, output }) => {
+                it(
+                    `input is ${JSON.stringify(input)}, output is ${output}`,
+                    () => {
+                        assert.equal(resolve(input.nums, input.k), output);
+                    }
+                );
+            }
+        );
+    }
+);
