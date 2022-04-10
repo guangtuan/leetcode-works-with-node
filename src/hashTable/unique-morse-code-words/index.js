3264a6cdf68b3519ae497f5e47a160c42df677c0
guangtuan <zhuangguangtuan@gmail.com>
2022-04-10 21:42:29 +0800

    unique-morse-code-words#804

---
+++ src/hashTable/unique-morse-code-words/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+    const dict = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+    let basic = "a".charCodeAt(0);
+
+    const translate = (word) => word.split("").map(c => c.charCodeAt(0) - basic).map(index => dict[index]).join("");
+
+    return words
+        .map(translate)
+        .reduce((acc, val) => !acc.add(val) || acc, new Set())
+        .size;
+};
+
+module.exports = uniqueMorseRepresentations;
---
+++ src/hashTable/unique-morse-code-words/test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const resolve = require('./index');
+
+const testCases = [
+    {
+        input: ["gin", "zen", "gig", "msg"],
+        expected: 2
+    },
+    {
+        input: ["a"],
+        expected: 1
+    }
+];
+
+describe('unique-morse-code-words', () => {
+    testCases.forEach(({input, expected}) => {
+        console.table([input]);
+        it('', () => {
+            let actual = resolve(input);
+            assert.deepEqual(
+                actual,
+                expected
+            );
+        });
+    });
+});
