79eb97d5f46de49581bacef376ff65429c587c86
guangtuan <zhuangguangtuan@gmail.com>
2022-04-12 08:03:45 +0800

    count-numbers-with-unique-digits#357

---
+++ src/math/count-numbers-with-unique-digits/test.js
@@ -0,0 +1,29 @@
+const resolve = require('./index');
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: 2,
+        expected: 91
+    },
+    {
+        input: 0,
+        expected: 1
+    },
+    {
+        input: 1,
+        expected: 10
+    },
+    {
+        input: 3,
+        expected: 739
+    }
+];
+
+describe("#357 count-numbers-with-unique-digits", () => {
+    testCases.forEach(({input, expected}) => {
+        it(`input is ${input} and expected is ${expected}`, () => {
+            assert.equal(resolve(input), expected);
+        });
+    });
+});
---
+++ src/math/count-numbers-with-unique-digits/testGetEachDigits.js
@@ -0,0 +1,21 @@
+const resolve = require('./getEachDigits');
+const assert = require('assert');
+
+const testCases = [
+    {
+        input: 91,
+        output: [9, 1]
+    },
+    {
+        input: 100,
+        output: [1, 0, 0]
+    }
+];
+
+describe("#helper get each digits", () => {
+    testCases.forEach(({input, output}) => {
+        it(`input is ${input} and output is ${output}`, () => {
+            assert.deepEqual(resolve(input), output);
+        });
+    });
+});
