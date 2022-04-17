30c6799adfd006e660a306724ce3154ea41f9ae9
guangtuan <zhuangguangtuan@gmail.com>
2022-04-17 21:55:42 +0800

    前缀和2题

---
+++ src/prefixsum/range-sum-query-immutable/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+    this.inner = [0];
+    for (let i = 1; i <= nums.length; i++) {
+        this.inner[i] = this.inner[i - 1] + nums[i - 1];
+    }
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+    return this.inner[right + 1] - this.inner[left];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+
+module.exports = NumArray;
---
+++ src/prefixsum/range-sum-query-immutable/test.js
@@ -0,0 +1,35 @@
+const assert = require("assert");
+const NumArray = require("./index");
+
+const testCases = [
+    {
+        nums: [-2, 0, 3, -5, 2, -1],
+        actions: [
+            {
+                input: [0, 2],
+                expected: 1
+            },
+            {
+                input: [2, 5],
+                expected: -1
+            },
+            {
+                input: [0, 5],
+                expected: -3
+            }
+        ]
+    }
+];
+
+describe('#range-sum-query-immutable', () => {
+    testCases.forEach(({nums, actions}) => {
+        console.table([nums]);
+        const numArray = new NumArray(nums);
+        actions.forEach(({input, expected}) => {
+            it(`input is ${input}, expected is ${expected}`, () => {
+                let actual = numArray.sumRange(input[0], input[1]);
+                assert.deepEqual(actual, expected);
+            });
+        });
+    });
+});
