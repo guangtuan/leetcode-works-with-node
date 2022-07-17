ebcde3e16c602b82d68c0b28435c672c782ee457
guangtuan <zhuangguangtuan@gmail.com>
2022-07-17 21:59:50 +0800

    weekly contest #302#3 refactor
    字符串比较写法修改
    重构成一串

--- src/weekly-contest-302/index3.js
+++ src/weekly-contest-302/index3.js
@@ -4,29 +4,10 @@
  * @return {number[]}
  */
 var smallestTrimmedNumbers = function (nums, queries) {
-    function compareNumberStr(numberStr1, numberStr2) {
-        for (let i = 0; i < numberStr1.length; i++) {
-            let val1 = numberStr1[i];
-            let val2 = numberStr2[i];
-            if (val1 !== val2) {
-                return val1 - val2;
-            }
-        }
-        return 0;
-    }
-
-    return queries.map(query => {
-        let [arrange, position] = query;
-        const withIndex = nums.map((input, index) => {
-            const cut = input.substring(input.length - position, input.length);
-            return {
-                value: cut,
-                index
-            }
-        });
-        withIndex.sort((ele1, ele2) => compareNumberStr(ele1.value, ele2.value));
-        return withIndex[arrange - 1].index;
-    })
+    return queries.map(([k, trim]) => nums
+        .map((input, index) => ({value: input.slice(-trim), index}))
+        .sort((ele1, ele2) => ele1.value === ele2.value ? 0 : ele1.value > ele2.value ? 1 : -1)
+        [k - 1].index);
 };
 
 module.exports = smallestTrimmedNumbers;
