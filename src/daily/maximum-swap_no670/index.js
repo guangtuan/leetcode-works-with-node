656265438f34700df5b8487f7a4d46fc7129a7d2
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-13 22:46:51 +0800

    daily -> no670 refactor: 重构为不可变写法

--- src/daily/maximum-swap_no670/index.js
+++ src/daily/maximum-swap_no670/index.js
@@ -3,47 +3,29 @@
  * @return {number}
  */
 var maximumSwap = function (num) {
-    const toEachBit = num => {
-        let curr = num;
-        let ret = [];
-        while (curr) {
-            ret.unshift(curr % 10);
-            curr = Math.floor(curr / 10);
-        }
-        return ret;
-    };
+    const split = n => (n < 10 ? [n] : [...split(Math.floor(n / 10)), n % 10]);
 
-    const getIndexOfMaxValue = (array, head) => {
-        let ret = head + 1;
-        let max = array[ret];
-        for (let i = ret + 1; i < array.length; i++) {
-            // 基于 `每一个大数字都很宝贵，得让他留在前面`
-            // 后边如果有等于 max 的，也要成为 max
-            if (array[i] >= max) {
-                max = array[i];
-                ret = i;
-            }
-        }
-        return ret;
-    };
+    const getIndexOfMaxValue = array =>
+        array.reduce((acc, v, index, arr) => (v >= arr[acc] ? index : acc), 0);
+
     // 如果 arr[0] 最大，则处理 arr[1:]
     // 处理过程：
     //    把 a[0] 和 max 交换
-    const handle = (array, head) => {
-        if (head === array.length - 1) {
-            return;
+    const swap = (array, head) => {
+        if (array.length === 1) {
+            return array;
         }
-        const index = getIndexOfMaxValue(array, head);
-        if (array[head] >= array[index]) {
-            handle(array, head + 1);
-            return;
+        const remain = array.slice(1);
+        const index = getIndexOfMaxValue(remain);
+        if (array[0] >= remain[index]) {
+            return [array[0], ...swap(remain, head + 1)];
         }
-        // swap
-        [array[index], array[head]] = [array[head], array[index]];
+        return [
+            remain[index],
+            ...remain.map((value, i) => (i == index ? array[0] : value)),
+        ];
     };
-    const eachBit = toEachBit(num);
-    handle(eachBit, 0);
-    return eachBit.reduce(
+    return swap(split(num), 0).reduce(
         (acc, v, index, arr) => acc + 10 ** (arr.length - index - 1) * v,
         0
     );
