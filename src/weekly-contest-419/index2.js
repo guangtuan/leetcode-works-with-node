5047581a397eafc1ff621017a458883e3f38f928
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-10-17 16:02:01 +0800

    feat: weekly-contest#419.2

---
+++ src/weekly-contest-419/index2.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestPerfectSubtree = function (root, k) {
+    const result = [];
+    const walk = (node) => {
+        if (node === null) {
+            result.push({perfect: true, size: 0});
+            return {perfect: true, size: 0};
+        }
+        let { perfect: leftPerfect, size: leftSize } = walk(node.left);
+        let { perfect: rightPerfect, size: rightSize } = walk(node.right);
+        const merged = {
+            perfect: leftPerfect && rightPerfect && (leftSize === rightSize),
+            size: leftSize + rightSize + 1
+        };
+        result.push(merged);
+        return merged;
+    }
+    walk(root);
+    return result
+        .filter(it => it.size !== 0 && it.perfect)
+        .sort((a, b) => b.size - a.size)
+        .map(it => it.size)
+    [k - 1] || -1;
+};
+
+module.exports = kthLargestPerfectSubtree;
--- src/weekly-contest-419/test.js
+++ src/weekly-contest-419/test.js
@@ -41,3 +41,65 @@ const assert = require('assert');
         })
     })
 })();
+
+(() => {
+    const resolve = require('./index2');
+    const testCases = [
+        {
+            input: {
+                root: [5, 3, 6, 5, 2, 5, 7, 1, 8, null, null, 6, 8],
+                k: 2
+            },
+            expected: 3,
+        },
+        {
+            input: {
+                root: [1, 2, 3, 4, 5, 6, 7],
+                k: 1
+            },
+            expected: 7,
+        },
+        {
+            input: {
+                root: [1, 2, 3, null, 4],
+                k: 3
+            },
+            expected: -1,
+        }
+    ]
+
+    function TreeNode(val, left, right) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+
+    const toTreeNode = (idx, array) => {
+        const value = array[idx];
+        if (value === null) {
+            return undefined;
+        }
+        if (value === undefined) {
+            return null;
+        }
+        let leftIndex = idx * 2 + 1;
+        let rightIndex = idx * 2 + 2;
+        return new TreeNode(
+            value,
+            toTreeNode(leftIndex, array),
+            toTreeNode(rightIndex, array)
+        );
+    }
+
+    describe('#2nd', () => {
+        testCases.forEach(({ input, expected }, index) => {
+            it(`case ${index}`, () => {
+                const label = `weekly-contest#418 -> case${index}`
+                console.time(label)
+                const actual = resolve(toTreeNode(0, input.root), input.k);
+                console.timeLog(label)
+                assert.deepEqual(actual, expected)
+            })
+        })
+    })
+})();
