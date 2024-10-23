dfa0bc5674b9a9f083692950dc77ceff0bc76fd3
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-10-23 17:03:26 +0800

    learning(bit): resolve 191

---
+++ src/bit/number-of-1-bits-191/index.ts
@@ -0,0 +1,9 @@
+function hammingWeight(n: number): number {
+    let ans = 0;
+    for (let i = 31; i >= 0; i--) {
+        ans += ((n >> i) & 1);
+    }
+    return ans;
+};
+
+module.exports = hammingWeight;
---
+++ src/bit/number-of-1-bits-191/test.ts
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const resolve = require('./index')
+
+const testCases = [
+    {
+        input: 11,
+        expected: 3
+    },
+    {
+        input: 128,
+        expected: 1
+    },
+    {
+        input: 2147483645,
+        expected: 30
+    }
+];
+
+const run = ({ input, expected }: { input: number, expected: number }) => {
+    it(`input is ${input}, expected is ${expected}`, () => {
+        assert.equal(resolve(input), expected);
+    });
+};
+
+describe('number of 1 bits', () => {
+    testCases.forEach(run);
+});
