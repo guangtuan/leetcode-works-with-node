31e9ba7608a067107a696768c72ef2ae7f14ec95
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-10-25 16:15:38 +0800

    learning(bit): resolve 693

---
+++ src/bit/binary-number-with-alternating-bits-693/index.ts
@@ -0,0 +1,27 @@
+function hasAlternatingBits(n: number): boolean {
+    // 没有连续的 0或者1，和右移后的元素进行异或，得到的是全 1 的数字
+    const x = n ^ (n >> 1);
+    // pattern of 111111....1
+    // 验证是否全1
+    return (x & (x + 1)) === 0;
+};
+
+const fn2 = (n: number): boolean => {
+    const getBitCount = (m: number): number => {
+        if (m === 0) {
+            return 1;
+        }
+        return Math.floor(Math.log2(m)) + 1;
+    }
+    console.log(`n is ${n.toString(2)}, and test ${(n ^ (n >> 1)).toString(2)}`);
+    // 没有相邻的1
+    if ((n & (n >> 1)) === 0) {
+        const mask = Math.pow(2, getBitCount(n)) - 1;
+        const re = n ^ mask;
+        // 取反后再次 验证有没有相邻的 1
+        return ((re & (re >> 1)) === 0);
+    }
+    return false;
+}
+
+module.exports = hasAlternatingBits;
