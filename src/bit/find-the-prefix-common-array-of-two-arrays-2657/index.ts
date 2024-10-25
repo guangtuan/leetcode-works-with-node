3acac3effe1ef93921d2e8fff671b271c7265aa0
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-10-25 17:35:00 +0800

    learning(bit): resolve 2657

---
+++ src/bit/find-the-prefix-common-array-of-two-arrays-2657/index.ts
@@ -0,0 +1,23 @@
+function findThePrefixCommonArray(A: number[], B: number[]): number[] {
+    const getBit1 = (m: bigint): number => {
+        let bit1Count = 0;
+        while (m > 0) {
+            if ((m & 1n) === 1n) {
+                bit1Count += 1;
+            }
+            m = m >> 1n;
+        }
+        return bit1Count;
+    }
+    let aValue = 0n;
+    let bValue = 0n;
+    const ans = [];
+    for (let i = 0; i < A.length; i++) {
+        aValue += 2n << BigInt(A[i]);
+        bValue += 2n << BigInt(B[i]);
+        ans.push(getBit1(aValue & bValue));
+    }
+    return ans;
+};
+
+module.exports = findThePrefixCommonArray;
