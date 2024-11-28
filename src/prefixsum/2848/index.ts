9199a291c9fe296ff3e761361e8a56afd753f0b3
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-11-28 11:12:17 +0800

    learning(diff): refactor 2848 with reduce

--- src/prefixsum/2848/index.ts
+++ src/prefixsum/2848/index.ts
@@ -1,22 +1,19 @@
 function numberOfPoints(nums: number[][]): number {
     const maxEnd = Math.max(...nums.map((it) => it[1]))
-    const diff = Array(maxEnd + 2).fill(0)
-    for (let [start, end] of nums) {
-        // 上车点记录一个标记
-        diff[start] += 1
-        // 在 end 的下一站下车
-        diff[end + 1] -= 1
-    }
-    let sumD = 0
-    let ans = 0
-    for (let d of diff) {
-        // 按照标记，进行上车或者下车的动作
-        sumD += d
-        if (sumD > 0) {
-            ans += 1
-        }
-    }
-    return ans
+    const diff = nums.reduce((acc: number[], value: number[]): number[] => {
+        const [start, end] = value
+        acc[start] += 1
+        acc[end + 1] -= 1
+        return acc
+    }, Array(maxEnd + 2).fill(0))
+    return diff.reduce(
+        (r: { ans: number; sumD: number }, value: number) => {
+            r.sumD += value
+            r.ans += r.sumD > 0 ? 1 : 0
+            return r
+        },
+        { ans: 0, sumD: 0 },
+    ).ans
 }
 
 module.exports = numberOfPoints
