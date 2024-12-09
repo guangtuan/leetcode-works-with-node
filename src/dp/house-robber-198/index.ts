92d01f18b6b5a229331e34f3a72382d9a94aac44
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-12-09 18:10:52 +0800

    refactor: rename

--- src/dp/house-robber-198/index.ts
+++ src/dp/house-robber-198/index.ts
@@ -2,13 +2,13 @@
  * fn(i) = max(fn(i-1), fn(i-2)+nums[i])
  */
 function rob(nums: number[]): number {
-    const cache = Array(nums.length + 2).fill(0)
+    const f = Array(nums.length + 2).fill(0)
 
     nums.forEach((value: number, i: number) => {
-        cache[i + 2] = Math.max(cache[i + 1], cache[i] + value)
+        f[i + 2] = Math.max(f[i + 1], f[i] + value)
     })
 
-    return cache[cache.length - 1]
+    return f[f.length - 1]
 }
 
 module.exports = rob
